900aae4d3ef0e16c2a1c1bd4a763c52ba34a52a2
Kanny <kanny.ghafour87@gmail.com>
2024-02-25 18:19:12 -0600

    lab completed

--- server/app.py
+++ server/app.py
@@ -14,25 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bake = Bakery.query.all()
+    responce  = [baking.to_dict() for baking in bake]
+
+    return make_response(jsonify(responce), 200)
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bake = Bakery.query.filter(Bakery.id==id).all()
+    responce  = [baking.to_dict() for baking in bake]
+    return make_response(jsonify(responce), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bake = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    responce  = [baking.to_dict() for baking in bake]
+    return make_response(jsonify(responce), 200)
+
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = db.session.query(BakedGood).order_by(BakedGood.price.desc()).first()
+    
+    response = most_expensive.to_dict() if most_expensive else {}
+    
+    return make_response(jsonify(response), 200)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -19,9 +19,9 @@ class Bakery(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     baked_goods = db.relationship('BakedGood', backref='bakery')
-
+    
     def __repr__(self):
-        return f'<Bakery {self.name}>'
+        return f'<Bakery {self.name} {self.created_at} {self.updated_at}>'
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
